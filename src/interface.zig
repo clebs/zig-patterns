493d5b7f04ceb845a70374bb82f95e184d70a5db
Borja Clemente <borja.clemente@gmail.com>
2024-05-09 22:02:31 +0200

    Extend interface pattern: unions
    Signed-off-by: Borja Clemente <borja.clemente@gmail.com>

--- src/interface.zig
+++ src/interface.zig
@@ -1,36 +1,10 @@
 const std = @import("std");
 const assert = std.debug.assert;
 
-/// Node is an interface for any type that has an update method.
-const Node = struct {
-    ptr: *anyopaque,
-    updateFn: *const fn (self: *anyopaque) void,
+// Different ways of doing polymorphism with different trade-offs.
+// This is quite similar to interfaces and overlaps with it.
 
-    /// init creates a Node with the given type as inner pointer whose `update` function will be called when calling `Node.update`
-    pub fn init(pointer: anytype) Node {
-        // validate pointer
-        const T = @TypeOf(pointer);
-        assert(@typeInfo(T) == .Pointer); // chek the type is a pointer
-        assert(@typeInfo(T).Pointer.size == .One);
-        assert(@typeInfo(@typeInfo(T).Pointer.child) == .Struct);
-
-        const gen = struct {
-            fn update(ptr: *anyopaque) void {
-                const self: T = @ptrCast(@alignCast(ptr));
-                self.update();
-            }
-        };
-
-        return .{
-            .ptr = pointer,
-            .updateFn = gen.update,
-        };
-    }
-
-    pub fn update(self: Node) void {
-        self.updateFn(self.ptr);
-    }
-};
+// Base types
 
 /// Player satisfies the Node interface.
 /// Inspired by gamedev, we made the example with what typically would be all different game entities that need to be updated.
@@ -56,24 +30,89 @@ const Enemy = struct {
     }
 };
 
-/// This function accepts any type that satisfies the Node interface.
-/// This constraint is checked at compile time.
-fn updateNode(node: anytype) void {
-    node.update();
-}
+// 1. Tagged unions: best approach for speed and best fit for the language syntax.
+// Worse memory, each instance takes as much space as the biggest type in the union.
+// Usually fine if types are similar.
+// Less flexible since we have to have all supported types inside the union (might be possible to generate with comptime).
+const UnionNode = union(enum) {
+    player: *Player,
+    enemy: *Enemy,
 
-test "array of interfaces" {
+    pub fn update(self: UnionNode) void {
+        switch (self) {
+            inline else => |un| {
+                un.update();
+            },
+        }
+    }
+};
+
+test "union node" {
+    // vars here are declared on the stack but usually these would be allocated.
     var p = Player{ .counter = 0 };
     var e = Enemy{};
+    const nodes = [_]UnionNode{ UnionNode{ .player = &p }, UnionNode{ .enemy = &e } };
 
-    var nodes = [_]Node{ Node.init(&p), Node.init(&e) };
+    // print a new line at the beginning
+    std.debug.print("\n", .{});
 
     for (nodes) |n| {
-        updateNode(n);
+        n.update();
     }
 
     // update nodes a second time to see that the counter on player changes.
     for (nodes) |n| {
-        updateNode(n);
+        n.update();
+    }
+}
+
+// 2. VTable: slower than enum since it has to deref pointers but much more similar to interfaces in other languages.
+// More memory efficient if types implementing this vary in size.
+const Node = struct {
+    ptr: *anyopaque,
+    updateFn: *const fn (self: *anyopaque) void,
+
+    /// init creates a Node with the given type as inner pointer whose `update` function will be called when calling `Node.update`
+    pub fn init(pointer: anytype) Node {
+        // validate pointer
+        const T = @TypeOf(pointer);
+        assert(@typeInfo(T) == .Pointer); // chek the type is a pointer
+        assert(@typeInfo(T).Pointer.size == .One);
+        assert(@typeInfo(@typeInfo(T).Pointer.child) == .Struct);
+
+        const vtable = struct {
+            fn update(ptr: *anyopaque) void {
+                const self: T = @ptrCast(@alignCast(ptr));
+                self.update();
+            }
+        };
+
+        return .{
+            .ptr = pointer,
+            .updateFn = vtable.update,
+        };
+    }
+
+    pub fn update(self: Node) void {
+        self.updateFn(self.ptr);
+    }
+};
+
+test "vTable" {
+    var p = Player{ .counter = 0 };
+    var e = Enemy{};
+
+    const nodes = [_]Node{ Node.init(&p), Node.init(&e) };
+
+    // print a new line at the beginning
+    std.debug.print("\n", .{});
+
+    for (nodes) |n| {
+        n.update();
+    }
+
+    // update nodes a second time to see that the counter on player changes.
+    for (nodes) |n| {
+        n.update();
     }
 }
